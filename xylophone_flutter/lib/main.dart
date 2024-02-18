1fde2f574a93f3ca4a2dc7411d23b8479b32dda2
Aashish Kumar <akrs56@gmail.com>
2024-02-18 22:13:34 +0530

    completed the project
    Used the tutorial to learn about using packages. Returning widget in function.

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -7,20 +7,42 @@ void main() => runApp(const XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  void playSound(int soundNumber){
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Widget buildKey({color1,soundNumber}){
+    return Expanded(
+      child: Container(
+        color: color1,
+        child: TextButton(
+          onPressed:(){
+            playSound(soundNumber);
+          }, child: const Text(''),
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    final player = AudioPlayer();
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed:(){
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: const Text('Click ME'),
-            ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              buildKey(color1: Colors.red, soundNumber: 1 ),
+              buildKey(color1: Colors.orange, soundNumber: 2),
+              buildKey(color1: Colors.yellow, soundNumber: 3),
+              buildKey(color1: Colors.green, soundNumber: 4),
+              buildKey(color1: Colors.teal, soundNumber: 5),
+              buildKey(color1: Colors.blue, soundNumber: 6),
+              buildKey(color1: Colors.purple, soundNumber: 7),
+            ],
           ),
 
         ),

1eaea10034acaa481c30fdf72ec7e993d2deaa12
Aashish Kumar <akrs56@gmail.com>
2024-03-13 08:20:03 +0530

    added quiz brain
    added the questions and the required methods. (OOP concepts used)

---
+++ quizzler/lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:quizzler/quiz_brain.dart';
+import 'package:flutter/material.dart';
+QuizBrain quizBrain = QuizBrain();
+void main() => runApp(const Quizzler());
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+
+  void checkans(bool userPickedAnswer){
+    bool correctAns = quizBrain.questAns();
+    if(correctAns==userPickedAnswer){
+      scoreKeeper.add(Icon(Icons.check, color: Colors.green,));
+    }
+    else{
+      scoreKeeper.add(Icon(Icons.close, color: Colors.red,));
+    }
+    setState(() {
+      quizBrain.nextQuestion();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Text(quizBrain.questionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                  )),
+              onPressed: () {
+                checkans(true);
+              },
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                ),
+                backgroundColor: const MaterialStatePropertyAll(Colors.red),
+              ),
+              onPressed: () {
+                checkans(false);// The user picked False
+              },
+              child: const Text(
+                'False',
+                style: TextStyle(fontSize: 20.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+        // TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+}
